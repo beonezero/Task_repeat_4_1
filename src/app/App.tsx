20daa34fe6f44d0f72945e9a823a3f59c551c6a6
Neakhai Yauheni <beonezero@protonmail.com>
2024-01-26 23:46:24 +0300

    add logOutTC

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -17,7 +17,7 @@ import {Login} from "../features/Login/Login";
 import {logOutTC, meTC} from "../auth/auth-reducer";
 import {CircularProgress} from "@mui/material";
 
-export const  App = () => {
+export const App = () => {
     const status = useAppSelector<RequestStatusType>(state => state.app.status)
     const isInitialized = useAppSelector<boolean>(state => state.app.isInitialized)
     const isLoggedIn = useAppSelector<boolean>(state => state.auth.isLoggedIn)
