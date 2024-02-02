0838a18ecf7a49f7f561eb595b886e81a2074bc4
Neakhai Yauheni <beonezero@protonmail.com>
2024-02-02 23:15:59 +0300

    refactoring auth-reducer.ts (RTC)

--- src/auth/auth-reducer.ts
+++ src/auth/auth-reducer.ts
@@ -1,71 +1,72 @@
 //types
-import {LoginDataType} from "../features/Login/Login";
-import {Dispatch} from "redux";
-import {setAppStatus, setIsInitialized} from "../app/app-reducer";
-import {authAPI} from "../api/todolist-api";
-import {handleNetworkAppError, handleServerAppError} from "../utils/error-utils";
-import {clearState} from "../features/TodolistList/todolists-reducer";
+import { LoginDataType } from "../features/Login/Login";
+import { Dispatch } from "redux";
+import { setAppStatus, setIsInitialized } from "../app/app-reducer";
+import { authAPI } from "../api/todolist-api";
+import {
+  handleNetworkAppError,
+  handleServerAppError,
+} from "../utils/error-utils";
+import { clearState } from "../features/TodolistList/todolists-reducer";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-type InitialAuthStateType = {
-    isLoggedIn: boolean
-}
+const initialState = {
+  isLoggedIn: false as boolean,
+};
 
-export type SetIsLoggedInStatusType = ReturnType<typeof setIsLoggedInStatus>
+const slice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  reducers: {
+    setIsLoggedInStatus: (
+      state,
+      action: PayloadAction<{ isLoggedIn: boolean }>
+    ) => {
+      state.isLoggedIn = action.payload.isLoggedIn;
+    },
+  },
+});
 
-type ActionAuthType = SetIsLoggedInStatusType
-
-const initialState: InitialAuthStateType = {
-    isLoggedIn: false
-}
-export const authReducer = (state: InitialAuthStateType = initialState, action: ActionAuthType): InitialAuthStateType => {
-    switch (action.type) {
-        case "AUTH/SET-ISLOGGEDIN-STATUS": {
-            return {...state, isLoggedIn: action.status}
-        }
-        default:
-            return state
-    }
-}
-export const setIsLoggedInStatus = (status: boolean) => ({type: "AUTH/SET-ISLOGGEDIN-STATUS", status}) as const
-
+export const authReducer = slice.reducer;
+export const authActions = slice.actions;
 export const logOutTC = () => async (dispatch: Dispatch) => {
-    setAppStatus("loading")
-    try {
-        const res = await authAPI.logOut()
-        if (res.data.resultCode === 0) {
-            dispatch(setIsLoggedInStatus(false))
-            dispatch(setAppStatus("succeeded"))
-            dispatch(clearState())
-        } else handleServerAppError(res.data, dispatch)
-    } catch (e) {
-        handleNetworkAppError(e, dispatch)
-    }
-}
+  setAppStatus("loading");
+  try {
+    const res = await authAPI.logOut();
+    if (res.data.resultCode === 0) {
+      dispatch(authActions.setIsLoggedInStatus({ isLoggedIn: false }));
+      dispatch(setAppStatus("succeeded"));
+      dispatch(clearState());
+    } else handleServerAppError(res.data, dispatch);
+  } catch (e) {
+    handleNetworkAppError(e, dispatch);
+  }
+};
 
 export const loginTC = (data: LoginDataType) => async (dispatch: Dispatch) => {
-    setAppStatus("loading")
-    try {
-        const res = await authAPI.loginIn(data)
-        if (res.data.resultCode === 0) {
-            dispatch(setIsLoggedInStatus(true))
-            dispatch(setAppStatus("succeeded"))
-        } else handleServerAppError(res.data, dispatch)
-    } catch (e) {
-        handleNetworkAppError(e, dispatch)
-    }
-}
+  setAppStatus("loading");
+  try {
+    const res = await authAPI.loginIn(data);
+    if (res.data.resultCode === 0) {
+      dispatch(authActions.setIsLoggedInStatus({ isLoggedIn: true }));
+      dispatch(setAppStatus("succeeded"));
+    } else handleServerAppError(res.data, dispatch);
+  } catch (e) {
+    handleNetworkAppError(e, dispatch);
+  }
+};
 
 export const meTC = () => async (dispatch: Dispatch) => {
-    setAppStatus("loading")
-    try {
-        const res = await authAPI.me()
-        if (res.data.resultCode === 0) {
-            dispatch(setIsLoggedInStatus(true))
-            dispatch(setAppStatus("succeeded"))
-        } else handleServerAppError(res.data, dispatch)
-    } catch (e) {
-        handleNetworkAppError(e, dispatch)
-    } finally {
-        dispatch(setIsInitialized(true))
-    }
-}
+  setAppStatus("loading");
+  try {
+    const res = await authAPI.me();
+    if (res.data.resultCode === 0) {
+      dispatch(authActions.setIsLoggedInStatus({ isLoggedIn: true }));
+      dispatch(setAppStatus("succeeded"));
+    } else handleServerAppError(res.data, dispatch);
+  } catch (e) {
+    handleNetworkAppError(e, dispatch);
+  } finally {
+    dispatch(setIsInitialized(true));
+  }
+};
